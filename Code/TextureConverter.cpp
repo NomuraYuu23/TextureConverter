dbac1d7442a7fe439904348f74d191d3caa37b22
野村優羽 <@MSI>
2024-10-07 15:26:25 +0900

    TL2資料04まで

--- Code/TextureConverter.cpp
+++ Code/TextureConverter.cpp
@@ -1,4 +1,5 @@
 #include "TextureConverter.h"
+#include <wrl.h>
 
 TextureConverter::TextureConverter()
 {
@@ -10,4 +11,32 @@ TextureConverter::~TextureConverter()
 
 void TextureConverter::ConverterTextureWICToDDS(const std::string& filePath)
 {
+
+	LoadWICTextureFromFile(filePath);
+
+}
+
+void TextureConverter::LoadWICTextureFromFile(const std::string& filePath)
+{
+
+	// 1.ファイルパスをワイド文字列に変換
+	std::wstring wFilePath = ConvertString(filePath);
+
+	// 2.テクスチャを読み込む
+
+}
+
+std::wstring TextureConverter::ConvertString(const std::string& str)
+{
+	if (str.empty()) {
+		return std::wstring();
+	}
+
+	auto sizeNeeded = MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), NULL, 0);
+	if (sizeNeeded == 0) {
+		return std::wstring();
+	}
+	std::wstring result(sizeNeeded, 0);
+	MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), &result[0], sizeNeeded);
+	return result;
 }
--- Code/TextureConverter.h
+++ Code/TextureConverter.h
@@ -21,5 +21,22 @@ public:
 	/// <param name="filePath">ファイルパス</param>
 	void ConverterTextureWICToDDS(const std::string& filePath);
 
+private: 
+
+	/// <summary>
+	/// テクスチャファイル読み込み
+	/// </summary>
+	/// <param name="filePath"></param>
+	void LoadWICTextureFromFile(const std::string& filePath);
+
+private:
+
+	/// <summary>
+	/// マルチバイト文字列をワイド文字列に変換
+	/// </summary>
+	/// <param name="str">マルチバイト文字列</param>
+	/// <returns>ワイド文字列</returns>
+	static std::wstring ConvertString(const std::string& str);
+
 };
 
